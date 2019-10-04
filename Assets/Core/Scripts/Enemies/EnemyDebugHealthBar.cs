54891e428732b368f21ac7733fedf3e89b443d04
ChickenMechanics <ChickenMechanics@gmail.com>
2019-10-04 23:01:50 +0200

    Updated debug health bar

--- Assets/Core/Scripts/Enemies/EnemyDebugHealthBar.cs
+++ Assets/Core/Scripts/Enemies/EnemyDebugHealthBar.cs
@@ -9,19 +9,23 @@ public class EnemyDebugHealthBar : MonoBehaviour
     private ScoreEnemyBasic m_ScoreEnemyBasicScr;
     private Image m_HealthBar;
     private Vector3 m_PosOffset;
+    
 
 
     private void Awake()
     {
+        transform.Find("HealthCanvas").GetComponent<Canvas>().worldCamera = Camera.main;
         m_ScoreEnemyBasicScr = transform.parent.GetComponent<ScoreEnemyBasic>();
         m_HealthBar = transform.Find("HealthCanvas").transform.Find("HealthImage").GetComponent<Image>();
+
+        m_HealthBar.transform.forward = transform.parent.forward;
         m_PosOffset = new Vector3(0.0f, 1.75f, 0.0f);
+        m_HealthBar.transform.position += m_PosOffset;
     }
 
 
     private void Update()
     {
         m_HealthBar.fillAmount = m_ScoreEnemyBasicScr.GetCurrentHealth / m_ScoreEnemyBasicScr.GetBaseHealth;
-        m_HealthBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + m_PosOffset);
     }
 }
