56b72a4e9c7df1c97e4ab38c25b11168666f45b3
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-10-07 17:14:27 +0200

    Vfx instance scene folder GameObject... or something

--- Assets/Core/Scripts/Enemies/EnemyVFX.cs
+++ Assets/Core/Scripts/Enemies/EnemyVFX.cs
@@ -19,12 +19,22 @@ public class EnemyVFX : MonoBehaviour
 
     private void Awake()
     {
+        GameObject parent = GameObject.Find("VfxFolder");
+        if(parent == null)
+        {
+            parent = new GameObject("VfxFolder");
+            parent.transform.rotation = Quaternion.identity;
+            parent.transform.position = Vector3.zero;
+        }
+
         if (m_ExplosionFireVfx != null)
         {
             m_ExplosionFireParticle = Instantiate(m_ExplosionFireVfx.GetComponent<ParticleSystem>(), transform.position, Quaternion.identity);
             m_ExplosionFireParticle.Stop();
             m_ExplosionFireParticle.transform.position = new Vector3(0.0f, -10.0f, 0.0f);
             m_ExplosionFireParticle.transform.localScale = new Vector3(m_ExplosionFireScale, m_ExplosionFireScale, m_ExplosionFireScale);
+
+            m_ExplosionFireParticle.transform.parent = parent.transform;
         }
 
         if (m_ExplosionSmokeVfx != null)
@@ -33,6 +43,8 @@ public class EnemyVFX : MonoBehaviour
             m_ExplosionSmokeParticle.Stop();
             m_ExplosionSmokeParticle.transform.position = new Vector3(0.0f, -10.0f, 0.0f);
             m_ExplosionSmokeParticle.transform.localScale = new Vector3(m_ExplosionSmokeScale, m_ExplosionSmokeScale, m_ExplosionSmokeScale);
+
+            m_ExplosionSmokeParticle.transform.parent = parent.transform;
         }
     }
 
