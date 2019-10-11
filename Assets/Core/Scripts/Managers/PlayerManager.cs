7c666815c398cd82a29cbf53ee10d9fc4f20a3e9
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-10-11 15:08:07 +0200

    Adde screen shakes for player taking damage

--- Assets/Core/Scripts/Managers/PlayerManager.cs
+++ Assets/Core/Scripts/Managers/PlayerManager.cs
@@ -9,6 +9,7 @@ public class PlayerManager : MonoBehaviour
 
     #region design vars
     public float m_BaseHealth = 100.0f;
+    public float m_TakeDmgShake = 0.5f;
     #endregion
     
     [HideInInspector]
@@ -25,6 +26,11 @@ public class PlayerManager : MonoBehaviour
     public float GetCurrentHealth { private set; get; }
     [HideInInspector]
     public bool GetIsAlive { private set; get; }
+
+    // screen shakes
+    private float m_ShakePrevHealth;
+    private float m_ShakeStartTime;
+    private float m_ShakeTime;
 
 
     //----------------------------------------------------------------------------------------------------
@@ -56,6 +62,28 @@ public class PlayerManager : MonoBehaviour
         GetCurrentHealth = GetBaseHealth;
 
         GetIsAlive = true;
+
+        m_ShakePrevHealth = GetCurrentHealth;
+        m_ShakeStartTime = Time.time;
+        m_ShakeTime = 0.25f;
+    }
+
+
+    private IEnumerator PlayerScreenShake()
+    {
+        while (Time.time < m_ShakeStartTime + m_ShakeTime)
+        {
+            Vector3 ranPos = new Vector3(Random.Range(-m_TakeDmgShake, m_TakeDmgShake),
+                Random.Range(-m_TakeDmgShake, m_TakeDmgShake),
+                Random.Range(-m_TakeDmgShake, m_TakeDmgShake));
+
+            Vector3 nowPos = GetPlayerLookScr.gameObject.transform.position;
+            GetPlayerLookScr.gameObject.transform.position = nowPos + ranPos;
+
+            yield return null;
+        }
+
+        StopCoroutine(PlayerScreenShake());
     }
 
 
@@ -87,6 +115,14 @@ public class PlayerManager : MonoBehaviour
             Cursor.visible = true;
 
             //LevelManager.GetInstance.ChangeScene(LevelManager.EScene.END);
+        }
+
+        // shakes
+        if(GetCurrentHealth != m_ShakePrevHealth)
+        {
+            m_ShakePrevHealth = GetCurrentHealth;
+            m_ShakeStartTime = Time.time;
+            StartCoroutine(PlayerScreenShake());
         }
     }
 }
