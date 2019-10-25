b14213c37e7feb455b4e7e1918ca10c882b64c86
Jonathan Berglin <jonis9898@hotmail.com>
2019-10-25 15:34:49 +0200

    Update Boomer behaviour

--- Assets/Core/Scripts/Enemies/BoomerBehaviour.cs
+++ Assets/Core/Scripts/Enemies/BoomerBehaviour.cs
@@ -1,25 +1,91 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class BoomerBehaviour : DefaultGroundEnemyBehaviour
 {
-    private NavMeshAgent agent;
+    public float damageAmount;
+    public float scoreAmount;
+    public float attackDuration;
+    public float attackRange;
+    public float health;
+
+    private EnemyBomberAnimation m_Anims;
+    private NavMeshAgent m_Agent;
 
     // Start is called before the first frame update
     private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        m_Agent = GetComponent<NavMeshAgent>();
+        currentState = State.MOVE;
+        m_Anims = gameObject.GetComponent<EnemyBomberAnimation>();
+        hp = health;
+
+        //SoundManager.GetInstance.PlaySoundClip(SoundManager.ESoundClip.ENEMY_SPAWN, transform.position);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        MoveTowardsPlayer(transform, agent);
-
-        if (distanceToPlayer < 4)
+        switch (currentState)
         {
-            GetComponent<Renderer>().material.color = Color.red;
-            //Explode
+            case State.ATTACK: StartCoroutine(Attack()); break;
+            case State.DEATH: Death(); break;
+            case State.MOVE: Move(); break;
         }
+    }
+
+    private IEnumerator Attack()
+    {
+        m_Anims.SetAnim(EnemyBomberAnimation.EAnimBomber.EXPLODE);
+
+        yield return new WaitForSeconds(attackDuration);
+
+        UpdatePlayerPos();
+
+        UpdateDistanceToPlayer();
+        
+        if (distanceToPlayer <= attackRange)
+            PlayerManager.GetInstance.DecreaseHealth(damageAmount);
+
+        foreach (var enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            if (Mathf.Abs(enemy.transform.position.magnitude - transform.position.magnitude) < attackRange)
+                enemy.GetComponent<DefaultGroundEnemyBehaviour>().TakeDamage(damageAmount);
+        }
+
+        Destroy(transform.gameObject);
+    }
+
+    private void Death()
+    {
+        ScoreManager.GetInstance.AddComboPoints(scoreAmount);
+
+#if DEBUG
+        if (SoundManager.GetInstance != null)
+#endif
+        {
+            SoundManager.GetInstance.PlaySoundClip(SoundManager.ESoundClip.CRAWLER_DEATH, transform.position);
+        }
+
+        m_Anims.SetAnim(EnemyBomberAnimation.EAnimBomber.EXPLODE);
+
+        foreach (var enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            if (Mathf.Abs(enemy.transform.position.magnitude - transform.position.magnitude) < attackRange)
+                enemy.GetComponent<DefaultGroundEnemyBehaviour>().TakeDamage(damageAmount);
+        }
+
+        Destroy(transform.gameObject);
+    }
+
+    private void Move()
+    {
+        m_Anims.SetAnim(EnemyBomberAnimation.EAnimBomber.WALK);
+
+        MoveTowardsPlayer(transform, m_Agent);
+
+        if (distanceToPlayer < attackRange)
+            currentState = State.ATTACK;
     }
 }
