e466d853f5ba8aefcd074b79e9e7d3f914c6033f
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-10-31 14:56:40 +0100

    Enemy flashing eye on projectile hit

--- Assets/Core/Scripts/Enemies/DefaultGroundEnemyBehaviour.cs
+++ Assets/Core/Scripts/Enemies/DefaultGroundEnemyBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class DefaultGroundEnemyBehaviour : MonoBehaviour
 {
@@ -16,11 +17,53 @@ public class DefaultGroundEnemyBehaviour : MonoBehaviour
     #endregion
 
     private GameObject m_PupilGO;
+    private float m_TargetEyeFlasherTime;
+    private float m_NowEyeFlasherTime;
+    private float m_EyeFlasherFunctionOnOff;
+    private float m_EyeFlasherFunctionOnOffTime;
+    private bool m_EyeOnOff;
+    private bool m_IsEyeFlash;
 
 
     private void Awake()
     {
         m_PupilGO = transform.GetChild(0).transform.GetChild(0).Find("Pupil").gameObject;
+        m_TargetEyeFlasherTime = 0.1f;
+        m_NowEyeFlasherTime = 0.0f;
+        m_EyeFlasherFunctionOnOffTime = 1.0f;
+        m_EyeFlasherFunctionOnOff = 0.0f;
+        m_EyeOnOff = false;
+        m_IsEyeFlash = false;
+    }
+
+
+    private void Update()
+    {
+        if(m_IsEyeFlash == true)
+        {
+            m_EyeFlasherFunctionOnOff += Time.deltaTime;
+            if(m_EyeFlasherFunctionOnOff > m_EyeFlasherFunctionOnOffTime)
+            {
+                m_EyeFlasherFunctionOnOff = 0.0f;
+                m_IsEyeFlash = false;
+                m_EyeOnOff = false;
+                m_PupilGO.SetActive(true);
+            }
+
+            EyeFlasherLogic();
+        }
+    }
+
+
+    private void EyeFlasherLogic()
+    {
+        m_NowEyeFlasherTime += Time.deltaTime;
+        if(m_NowEyeFlasherTime > m_TargetEyeFlasherTime)
+        {
+            m_NowEyeFlasherTime = 0.0f;
+            m_EyeOnOff = !m_EyeOnOff;
+            m_PupilGO.SetActive(m_EyeOnOff);
+        }
     }
 
 
@@ -68,9 +111,9 @@ public class DefaultGroundEnemyBehaviour : MonoBehaviour
                 {
                     SoundManager.GetInstance.PlaySoundClip(SoundManager.ESoundClip.CRAWLER_HURT, other.transform.position);
                 }
-            }
 
-            //m_PupilGO.SetActive(false);
+                m_IsEyeFlash = true;
+            }
 
             //TakeDamage(other.GetComponent<BulletBehaviour>().m_DamageValue);
         }
