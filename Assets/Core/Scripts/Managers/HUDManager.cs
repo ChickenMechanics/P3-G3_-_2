91a71a0243620152f4a17685fa0a41b74be106f6
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-11-04 10:23:07 +0100

    Exposed visor crack triggers to HUDmanager inspector

--- Assets/Core/Scripts/Managers/HUDManager.cs
+++ Assets/Core/Scripts/Managers/HUDManager.cs
@@ -43,6 +43,12 @@ public class HUDManager : MonoBehaviour
     public Color m_Full;
     public Color m_Semi;
     public Color m_Empty;
+
+    [Header("Crack economy")]
+    public float m_Crack_1;
+    public float m_Crack_2;
+    public float m_Crack_3;
+    public float m_Crack_4;
     #endregion
 
     public static HUDManager GetInstance { get; private set; }
@@ -353,7 +359,7 @@ public class HUDManager : MonoBehaviour
     {
         if(m_IsCrack[0] == false)
         {
-            if (PlayerManager.GetInstance.GetCurrentHealth < 80.1f)
+            if (PlayerManager.GetInstance.GetCurrentHealth <= m_Crack_1)
             {
                 m_IsCrack[0] = true;
                 m_PlayerCracks[0].gameObject.SetActive(true);
@@ -361,7 +367,7 @@ public class HUDManager : MonoBehaviour
         }
         else if (m_IsCrack[1] == false)
         {
-            if (PlayerManager.GetInstance.GetCurrentHealth < 60.1f)
+            if (PlayerManager.GetInstance.GetCurrentHealth <= m_Crack_2)
             {
                 m_IsCrack[1] = true;
                 m_PlayerCracks[1].gameObject.SetActive(true);
@@ -369,7 +375,7 @@ public class HUDManager : MonoBehaviour
         }
         else if (m_IsCrack[2] == false)
         {
-            if (PlayerManager.GetInstance.GetCurrentHealth < 40.1f)
+            if (PlayerManager.GetInstance.GetCurrentHealth <= m_Crack_3)
             {
                 m_IsCrack[2] = true;
                 m_PlayerCracks[2].gameObject.SetActive(true);
@@ -377,7 +383,7 @@ public class HUDManager : MonoBehaviour
         }
         else if (m_IsCrack[3] == false)
         {
-            if (PlayerManager.GetInstance.GetCurrentHealth < 20.1f)
+            if (PlayerManager.GetInstance.GetCurrentHealth <= m_Crack_4)
             {
                 m_IsCrack[3] = true;
                 m_PlayerCracks[3].gameObject.SetActive(true);
