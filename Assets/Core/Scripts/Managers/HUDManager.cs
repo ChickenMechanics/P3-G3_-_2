5544080005fb931af6ad2bc2482b929e29a34c9b
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-11-06 09:24:04 +0100

    Save commit

--- Assets/Core/Scripts/Managers/HUDManager.cs
+++ Assets/Core/Scripts/Managers/HUDManager.cs
@@ -382,6 +382,7 @@ public class HUDManager : MonoBehaviour
         {
             m_bHelmetFlashOnOff = false;
             m_NowHelmetFlashLightTime = m_HelmetLightOffTime;
+            Mathf.Lerp();
             m_LeftLightImg.color = Color.black;
             m_RightLightImg.color = Color.black;
         }
