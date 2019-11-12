79a403cdb92fb1dae40d79d1a45b3ff807364c49
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-11-12 13:19:46 +0100

    Highscore main menu functionality

--- Assets/Core/Scripts/GUI/ButtonFunctionality/MainMenuGUI.cs
+++ Assets/Core/Scripts/GUI/ButtonFunctionality/MainMenuGUI.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 
 public class MainMenuGUI : MonoBehaviour
 {
+    private Transform m_HighScoreTransform;
     private Transform m_HowToPlayTransform;
     private Transform m_CreditsTransform;
 
 
     private void Start()
     {
+        m_HighScoreTransform = transform.Find("HighScoreButton").gameObject.transform.Find("HighScoreImg").gameObject.transform;
+        m_HighScoreTransform.gameObject.SetActive(false);
+
         m_HowToPlayTransform = transform.Find("HowToPlayButton").gameObject.transform.Find("HowToPlayImg").gameObject.transform;
         m_HowToPlayTransform.gameObject.SetActive(false);
 
@@ -31,14 +35,40 @@ public class MainMenuGUI : MonoBehaviour
     }
 
 
-    public void Options()
+    public void HighScore()
     {
-        LevelManager.GetInstance.ChangeScene(LevelManager.EScene.OPTIONS);
+        if (m_HowToPlayTransform.gameObject.activeInHierarchy == true)
+        {
+            m_HowToPlayTransform.gameObject.SetActive(false);
+        }
+        if (m_CreditsTransform.gameObject.activeInHierarchy == true)
+        {
+            m_CreditsTransform.gameObject.SetActive(false);
+        }
+
+        if (m_HighScoreTransform.gameObject.activeInHierarchy == false)
+        {
+            m_HighScoreTransform.gameObject.SetActive(true);
+        }
+        else
+        {
+            m_HighScoreTransform.gameObject.SetActive(false);
+        }
     }
+
+
+    //public void Options()
+    //{
+    //    LevelManager.GetInstance.ChangeScene(LevelManager.EScene.OPTIONS);
+    //}
 
 
     public void HowToPlay()
     {
+        if (m_HighScoreTransform.gameObject.activeInHierarchy == true)
+        {
+            m_HighScoreTransform.gameObject.SetActive(false);
+        }
         if (m_CreditsTransform.gameObject.activeInHierarchy == true)
         {
             m_CreditsTransform.gameObject.SetActive(false);
@@ -57,6 +87,10 @@ public class MainMenuGUI : MonoBehaviour
 
     public void Credits()
     {
+        if (m_HighScoreTransform.gameObject.activeInHierarchy == true)
+        {
+            m_HighScoreTransform.gameObject.SetActive(false);
+        }
         if (m_HowToPlayTransform.gameObject.activeInHierarchy == true)
         {
             m_HowToPlayTransform.gameObject.SetActive(false);
