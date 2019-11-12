37a02b4ef2571a2f8beffb513e33f3fa94433a1b
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-11-12 13:09:26 +0100

    How to play functionality

--- Assets/Core/Scripts/GUI/ButtonFunctionality/MainMenuGUI.cs
+++ Assets/Core/Scripts/GUI/ButtonFunctionality/MainMenuGUI.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class MainMenuGUI : MonoBehaviour
 {
+    private Transform m_HowToPlayTransform;
+
+
+    private void Start()
+    {
+        m_HowToPlayTransform = transform.Find("HowToPlayButton").gameObject.transform.Find("HowToPlayImg").gameObject.transform;
+        m_HowToPlayTransform.gameObject.SetActive(false);
+    }
+
+
     public void NewGame()
     {
         ScoreManager.GetInstance.ResetPlayerStats();
@@ -19,6 +30,19 @@ public class MainMenuGUI : MonoBehaviour
     public void Options()
     {
         LevelManager.GetInstance.ChangeScene(LevelManager.EScene.OPTIONS);
+    }
+
+
+    public void HowToPlay()
+    {
+        if(m_HowToPlayTransform.gameObject.activeInHierarchy == false)
+        {
+            m_HowToPlayTransform.gameObject.SetActive(true);
+        }
+        else
+        {
+            m_HowToPlayTransform.gameObject.SetActive(false);
+        }
     }
 
 
