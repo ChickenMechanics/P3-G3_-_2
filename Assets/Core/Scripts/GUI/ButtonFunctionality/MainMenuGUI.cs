4a03fe65ff84503842bc9a172518f8e3e651befb
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-11-12 13:14:46 +0100

    Credits functionality

--- Assets/Core/Scripts/GUI/ButtonFunctionality/MainMenuGUI.cs
+++ Assets/Core/Scripts/GUI/ButtonFunctionality/MainMenuGUI.cs
@@ -7,12 +7,16 @@ using UnityEngine.UI;
 public class MainMenuGUI : MonoBehaviour
 {
     private Transform m_HowToPlayTransform;
+    private Transform m_CreditsTransform;
 
 
     private void Start()
     {
         m_HowToPlayTransform = transform.Find("HowToPlayButton").gameObject.transform.Find("HowToPlayImg").gameObject.transform;
         m_HowToPlayTransform.gameObject.SetActive(false);
+
+        m_CreditsTransform = transform.Find("CreditsButton").gameObject.transform.Find("CreditsImg").gameObject.transform;
+        m_CreditsTransform.gameObject.SetActive(false);
     }
 
 
@@ -35,13 +39,36 @@ public class MainMenuGUI : MonoBehaviour
 
     public void HowToPlay()
     {
-        if(m_HowToPlayTransform.gameObject.activeInHierarchy == false)
+        if (m_CreditsTransform.gameObject.activeInHierarchy == true)
+        {
+            m_CreditsTransform.gameObject.SetActive(false);
+        }
+
+        if (m_HowToPlayTransform.gameObject.activeInHierarchy == false)
         {
             m_HowToPlayTransform.gameObject.SetActive(true);
         }
         else
         {
             m_HowToPlayTransform.gameObject.SetActive(false);
+        }
+    }
+
+
+    public void Credits()
+    {
+        if (m_HowToPlayTransform.gameObject.activeInHierarchy == true)
+        {
+            m_HowToPlayTransform.gameObject.SetActive(false);
+        }
+
+        if (m_CreditsTransform.gameObject.activeInHierarchy == false)
+        {
+            m_CreditsTransform.gameObject.SetActive(true);
+        }
+        else
+        {
+            m_CreditsTransform.gameObject.SetActive(false);
         }
     }
 
